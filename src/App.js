42a9dea193b9fa60057ce15dfcdd2eb178c509dd
Caroline Strickland <carolinelstrickland@outlook.com>
2021-01-03 12:07:29 -0600

    creating page routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavBar from "../src/components/NavBar";
+import Landing from "../src/pages/Landing";
+import Links from "../src/pages/Links";
+import PrivacyPolicy from "../src/pages/PrivacyPolicy";
+import Team from "../src/pages/Team";
+import Services from "../src/pages/Services";
+
+
+
 
 function App() {
   return (
-    <div className="App">
-     <NavBar />
-    </div>
+    <React.Fragment>
+      <NavBar />
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/services" component={Services} />
+          <Route exact path="/links" component={Links} />
+          <Route exact path="/team" component={Team} />
+          <Route exact path="/privacypolicy" component={PrivacyPolicy} />
+       </div>
+      </Router>
+    </React.Fragment>
+
+
   );
 }
 
