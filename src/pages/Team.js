ea83275c27c40e5c33695c8d05e66c0984045353
Mariafcc <mariafernandacruz@MariaFernandas-MacBook-Pro.local>
2021-02-05 18:32:17 -0600

    changes to team

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,23 +1,22 @@
 import React, { useState, Component } from 'react';
 import TeamEmp from "../component/TeamEmp"
 import "../component/style/teamemp.css"
+import NavBar from '../component/NavBar';
 
 
 class Team extends Component {
-    
+
     render() {
 
         return (
             <div>
-            <img src="..//img/image003.jpg"
-              style={{height:"800px", width:"100%", opacity: .4 }}></img>
-              <div className="topLeft">
-            <TeamEmp />
-            </div>
+                <div>
+                    <TeamEmp />
+                </div>
             </div>
         );
 
-       
+
     }
 }
 
